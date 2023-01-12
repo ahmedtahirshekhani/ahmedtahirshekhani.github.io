f440d897351bc808bf9781579464919005ed18e8
Ahmed Tahir Shekhani <ahmedtahir2000@gmail.com>
2023-01-13 03:04:41 +0500

    build files

--- components/homecontent/index.tsx
+++ components/homecontent/index.tsx
@@ -4,7 +4,9 @@ const HomeContent = () => {
             <h1 className="text-4xl font-bold my-3">Ahmed Tahir Shekhani</h1>
             <hr />
             <div className="mt-5">
-            A passionate and experienced <em><strong className="text-primary">FULL-STACK DEVELOPER</strong></em> and a <em><strong className="text-primary">DIGITAL MARKETING CONSULTANT</strong></em> who can solve your technical problem in the easiest, most desirable, and promotional ways. I am a solution to your problem. I tell my clients to not look at my winners but look at my losers because they are very few ~ from The Wolf of Wall Street. I always believe in 100% quality service and time. Being an experienced strategist I can provide you any guidance on text or live chat. So don't hesitate to contact me.
+            <p>
+            A passionate and experienced <em><strong className="text-primary">FULL-STACK DEVELOPER</strong></em> and a <em><strong className="text-primary">DIGITAL MARKETING CONSULTANT</strong></em> who can solve your technical problem in the easiest, most desirable, and promotional ways. I am a solution to your problem. I tell my clients to not look at my winners but look at my losers because they are very few ~ from The Wolf of Wall Street. I always believe in 100% quality service and time. Being an experienced strategist I can provide you any guidance on text or live chat. So don`&apos;`t hesitate to contact me.
+            </p>
             </div>
         </div>
     )
