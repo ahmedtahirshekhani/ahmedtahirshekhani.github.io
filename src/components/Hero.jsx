7b4e47332b282a6e1a612f62fe89a70b8523bd90
Dude-tales <146628064+Dude-tales@users.noreply.github.com>
2023-12-26 21:23:32 +0500

    placed moving text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,8 +30,11 @@ const Hero = () => {
 
       <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center show'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 show'>
+        <div className="absolute text-white text-4xl font-semibold herodiv pt-40 mt-80">
+          <marquee>Welcome to My Portfolio Website.</marquee>
+        </div>
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center '>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 '>
             <motion.div
               animate={{
                 y: [0, 24, 0],
